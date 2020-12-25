44bb5fedbbd958edeaee423b0629098334834dfe
Ravikant Khandelwal <ravikant.khandelwal@bts.com>
2020-12-25 15:51:03 +0530

    Updates

---
+++ precache-manifest.4d2083aac960e4227656ebf481e7ccd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4a99fe74b76f56a56aa9561b6b018f",
+    "url": "/react-birthday-list/index.html"
+  },
+  {
+    "revision": "73c4e1d04189463e0c15",
+    "url": "/react-birthday-list/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/react-birthday-list/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-birthday-list/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c4e1d04189463e0c15",
+    "url": "/react-birthday-list/static/js/main.fe169243.chunk.js"
+  },
+  {
+    "revision": "80521359f6d7f5c94aae",
+    "url": "/react-birthday-list/static/js/runtime-main.2b71f430.js"
+  }
+]);
